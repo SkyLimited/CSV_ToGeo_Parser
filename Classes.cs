d973e366fd7e83c5f7f8d33e2353b28b5524265a
Кузнецов Андрей Сергеевич <skylimited@live.ru>
2022-02-23 12:28:40 +0300

    Добавьте файлы проекта.

---
+++ Classes.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToGeoParser
+{
+     public class GeoJSON
+    {
+        public string type { get; set; }
+        public List<Feature> features { get; private set; }
+        public CRS crs { get; private set; }
+
+        public GeoJSON()
+        {
+            type = "FeatureCollection";
+            features = new List<Feature>();
+            crs = new CRS();
+        }
+
+    }
+
+    public class Feature
+    {
+        public string type { get; set; }
+        public Geometry geometry { get; private set; }
+
+        public Feature()
+        {
+            type = "Feature";
+            geometry = new Geometry();
+        }
+    }
+
+    public class CRS
+    {
+        public string type { get; set; }
+        public Properties properties { get; private set; }
+
+        public CRS()
+        {
+            type = "name";
+            properties = new Properties();
+        }
+    }
+
+    public class Properties
+    {
+        public string name = "EPSG:3857";
+
+    }
+
+    public class Geometry
+    {
+        public string type { get; set; }
+        public List<List<double[]>> coordinates { get; private set; }
+
+        public void AddCoords(double x, double y)
+        {
+            var t = new double[2];
+            t[0] = x;
+            t[1] = y;
+            realCords.Add(t);
+            
+        }
+
+
+        public string SerializeAsWKT()
+        {
+            var res = "(";
+            for(int i=0; i< realCords.Count; i++)
+            {
+                if (i != 0)
+                    res += ", ";
+                res += realCords[i][0].ToString("0.000000", System.Globalization.CultureInfo.InvariantCulture) + " " + realCords[i][1].ToString("0.000000", System.Globalization.CultureInfo.InvariantCulture);
+                
+            }
+            res += ")";
+            return res;
+
+        }
+
+        private List<double[]> realCords;
+
+
+        public Geometry()
+        {
+            type = "Polygon";
+            coordinates = new List<List<double[]>>();
+            coordinates.Add(new List<double[]>());
+            realCords = coordinates.First();
+        }
+
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToGeoParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CSV to Geo Parser v 0.1\nUSAGE: csv_to_geo_parser.exe --type=[Poly|Line]  file1.csv file2.csv file3.csv ...");
+
+            //parse args
+            if(!args.Any(_=>_.StartsWith("--type")) ||                
+                args.Count() <2 
+              )
+            {
+                Console.WriteLine("All Arguments are mandatory.");
+                return;                
+            }
+
+            var mode = args.FirstOrDefault(_ => _.StartsWith("--type=")).Split('=')[1];
+            if(mode!="Poly" && mode != "Line")
+            {
+                Console.WriteLine($"mode {mode} is not supported.");
+                return;
+            }
+        /*    var outx = args.FirstOrDefault(_ => _.StartsWith("--out=")).Split('=')[1];
+            if(outx!="WKT" && outx!="GeoJSON")
+            {
+                Console.WriteLine($"out {outx} is not supported.");
+                return;
+            }*/
+
+            var fileList = args.Where(_ => !_.StartsWith("--")).ToList();
+            var gj = new GeoJSON();
+            foreach (var file in fileList)
+            {
+                var strings = File.ReadAllLines(file);
+
+
+
+                Feature feature = null;
+                var lasts = "";
+                foreach (var s in strings)
+                {
+                    if ((s != ";" && lasts == ";") || lasts == "")
+                    {
+                        feature = new Feature();
+                        gj.features.Add(feature);
+                    }
+                    if (s != ";")
+                    {
+                        var crds = s.Replace(".", ",").Split(';');
+                        feature.geometry.AddCoords(Double.Parse(crds[0]), Double.Parse(crds[1]));
+                    }
+                    lasts = s;
+                }
+            }
+                string res = "";
+                if (mode == "Poly")
+                {
+                    res = JsonConvert.SerializeObject(gj);
+
+                }
+                if (mode == "Line")
+                {
+                    res = "MULTILINESTRING (";
+                    for (int i = 0; i < gj.features.Count; i++)
+                    {
+                        var f = gj.features[i];
+                        if (i != 0)
+                            res += ", ";
+                        res += f.geometry.SerializeAsWKT();
+                    }
+                    res += ")";
+                }
+            File.WriteAllText("out.txt", res);
+            
+        }
+    }
+}
